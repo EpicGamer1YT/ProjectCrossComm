a51f08df4abdfa72e0be4c735d6cafa712a29bfe
Connor <cam-o-man@live.com>
2019-03-18 16:48:14 -0400

    Added cryptico and began generation of public-private keypairs
    Signed-off-by: Connor <cam-o-man@live.com>

--- chatJS.js
+++ chatJS.js
@@ -9,7 +9,55 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-function searchEmails() {
+var target;
+var local;
+
+function searchEmails(email) {
+    var remoteEmail;
+    var username;
+    database.ref("/users/emailConv/" + email).once().then(function(snapshot) {
+        //On completion
+       username = (snapshot.val() && snapshot.val().username);
+       remoteEmail  = (snapshot.val() && snapshot.val().email);
+    }).catch(function(error) {
+        console.log(error.message);
+        console.log(error.code);
+    });
+    if (remoteEmail.equals(email)) {
+        return remoteEmail;
+    } else {
+        return "No emails found";
+    }
+}
+function parseSearchedEmails() {
+    target = document.getElementById("findEmail").value;
     var email = document.getElementById("findEmail").value;
-    database.ref("/users/emailConv/" + email).once()
+    var modifiedEmail = email.replace(".", ",");
+    var returnEmail = searchEmails(modifiedEmail);
+    if (returnEmail.equals("No emails found")) {
+        document.getElementById("return").value = "No emails found";
+
+    } else {
+        document.getElementById("return").value = returnEmail;
+    }
+}
+
+function startChat() { //Will start an encrypted chat between two users
+
+}
+
+function generateKeyPair() {
+    
+}
+function showPassPhraseInput() {
+    let x = document.getElementById("hiddenPass");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function splitter() {
+    parseSearchedEmails();
+    generateKeyPair();
 }
