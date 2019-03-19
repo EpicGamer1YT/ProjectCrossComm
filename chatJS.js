fbbc5774d1aa2df799add382547a9d6d7fb24de8
Connor <cam-o-man@live.com>
2019-03-19 10:59:44 -0400

    added cryptico syncing to /chats/UID UID/UID/pubkey and /privkey
    Signed-off-by: Connor <cam-o-man@live.com>

--- chatJS.js
+++ chatJS.js
@@ -8,12 +8,11 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-
+var remoteEmail;
 var target;
 var local;
 
 function searchEmails(email) {
-    var remoteEmail;
     var username;
     database.ref("/users/emailConv/" + email).once().then(function(snapshot) {
         //On completion
@@ -30,7 +29,6 @@ function searchEmails(email) {
     }
 }
 function parseSearchedEmails() {
-    target = document.getElementById("findEmail").value;
     var email = document.getElementById("findEmail").value;
     var modifiedEmail = email.replace(".", ",");
     var returnEmail = searchEmails(modifiedEmail);
@@ -40,14 +38,33 @@ function parseSearchedEmails() {
     } else {
         document.getElementById("return").value = returnEmail;
     }
+
 }
 
-function startChat() { //Will start an encrypted chat between two users
-
+function startChat(user, userkey, userPubKey) { //Will start an encrypted chat between two users
+    target = database.ref("/users/emailConv/" + remoteEmail).once()
+    var targetUID = target.uid;
+    var localUID = firebase.auth().currentUser().uid;
+    database.ref("/chats/" + localUID + " " + targetUID + "/" + localUID + "/privkey").set({
+        "privkey": cryptico.privateKey(userkey).toString,
+    }).catch(function(error) {
+        console.log(error.message);
+        console.log(error.code);
+    });
+    database.ref("/chats/" + localUID + " " + targetUID + "/" + localUID + "/pubkey").set({
+        "pubkey": userPubKey.toString(),
+    }).catch(function(error) {
+        console.log(error.message);
+        console.log(error.code);
+    });
 }
 
 function generateKeyPair() {
-    
+    var passPhrase = document.getElementById("passInput").value;
+    var bits = 1024;
+    var userkey = cryptico.generateRSAKey(passPhrase, bits);
+    var userPubKey = cryptico.publicKeyString(userkey);
+    startChat(firebase.auth().currentUser(), userkey, userPubKey);
 }
 function showPassPhraseInput() {
     let x = document.getElementById("hiddenPass");
@@ -59,5 +76,5 @@ function showPassPhraseInput() {
 }
 function splitter() {
     parseSearchedEmails();
-    generateKeyPair();
+    showPassPhraseInput();
 }
