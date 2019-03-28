57c34961bebe011ba56f0d906f1d77493a7992c8
Connor McDermid <cam-o-man@live.com>
2019-03-28 18:09:14 -0400

    have to save work and leave for now: here are updated changes
    Signed-off-by: Connor McDermid <cam-o-man@live.com>

--- ChatJS.js
+++ ChatJS.js
@@ -11,11 +11,14 @@ firebase.initializeApp(config);
 require("firebase/app");
 require("firebase/auth");
 require("firebase/database");
+require("cryptico");
+const cryptico = require("cryptico");
 var database = firebase.database();
 var userkey;
 var remoteEmail;
 var target;
 var local;
+var localuuid = firebase.auth().currentUser().uid;
 var targetUID;
 function searchEmails(email) {
     var username;
@@ -40,24 +43,22 @@ function parseSearchedEmails() {
     var modifiedEmail = email.replace(".", ",");
     var returnEmail = searchEmails(modifiedEmail);
     if (returnEmail.equals("No emails found")) {
-        document.getElementById("return").value = "No emails found";
+        document.getElementById("listHere").value = "No users of that name found."; //replaces value of node
 
     } else {
-        document.getElementById("return").value = returnEmail;
+        // noinspection JSJQueryEfficiency
+        $("#listHere").append("Users found:"); //Adds to value of node
+        // noinspection JSJQueryEfficiency
+        $("#listHere").append(returnEmail);
     }
 
 }
 
-function startChat(user, userkey, userPubKey) { //Will start an encrypted chat between two users
-    target = database.ref("/users/emailConv/" + remoteEmail).once()
+function startChat(user, userkey, userPubKey) { //Will start an encrypted chat between two users FIXME: Needs rewriting
+    target = database.ref("/users/emailConv/" + remoteEmail).once();
     targetUID = target.uid;
     var localUID = firebase.auth().currentUser().uid;
-    database.ref("/chats/" + localUID + " " + targetUID + "/" + localUID + "/privkey").set({
-        "privkey": cryptico.privateKey(userkey).toString,
-    }).catch(function(error) {
-        console.log(error.message);
-        console.log(error.code);
-    });
+
     database.ref("/chats/" + localUID + " " + targetUID + "/" + localUID + "/pubkey").set({
         "pubkey": userPubKey.toString(),
     }).catch(function(error) {
@@ -66,31 +67,62 @@ function startChat(user, userkey, userPubKey) { //Will start an encrypted chat b
     });
 }
 
-function generateKeyPair() {
-    var passPhrase = document.getElementById("passInput").value;
-    var bits = 1024;
-    userkey = cryptico.generateRSAKey(passPhrase, bits);
-    var userPubKey = cryptico.publicKeyString(userkey);
-    startChat(firebase.auth().currentUser(), userkey, userPubKey);
-}
-function showPassPhraseInput() {
-    let x = document.getElementById("hiddenPass");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+/**
+ * uses Cryptico.js to generate a public/private keypair
+ */
+function generateKeyPair() { //TODO: Finish writing this and startChat()
+    var position = database.ref("/users/emailConv/" + local.email.replace(/\./g, "") + "/chats/" + targetUID).once();
+    if (position) {
+        database.ref("/chats/" + localuuid + " " + targetUID + "/" + localuuid).once().then( (snapshot) => {
+            if (typeof snapshot.val().privkey == null) {
+                var passPhrase = "Javascript is incredibly inconsistent."; //Is this visible in our code? Yes. Does it matter? No. It's seeded.
+                var bits = 1024;
+                userkeynew = cryptico.generateRSAKey(passPhrase, bits);
+                var userPubKeynew = cryptico.publicKeyString(userkeynew);
+                database.ref("/chats/" + localUID + " " + targetUID + "/" + localUID + "/privkey").set({
+                    "privkey": cryptico.privateKey(userkeynew).toString,
+                    "pubkey": userPubKeynew.toString(),
+                }).catch(function(error) {
+                    console.log(error.message);
+                    console.log(error.code);
+                });
+                startChat(firebase.auth().currentUser(), userkeynew, userPubKeynew);
+            } else {
+                var userkey = snapshot.val().privkey;
+                var userPubKey = snapshot.val().pubKey;
+                startChat(firebase.auth().currentUser(), userkey, userPubKey);
+            }
+        }).catch( (error) => {
+            console.log(error.message);
+            console.log(error.code);
+        });
     } else {
-        x.style.display = "none";
+        database.ref("/chats/" + targetUID + " " +localuuid + "/" + localuuid).once().then( (snapshot) => {
+            if (typeof snapshot.val().privkey == null) {
+                var passPhrase = "Javascript is incredibly inconsistent."; //Is this visible in our code? Yes. Does it matter? No. It's seeded.
+                var bits = 1024;
+                userkeynew = cryptico.generateRSAKey(passPhrase, bits);
+                var userPubKeynew = cryptico.publicKeyString(userkeynew);
+                startChat(firebase.auth().currentUser(), userkeynew, userPubKeynew);
+            } else {
+                var userkey = snapshot.val().privkey;
+                var userPubKey = snapshot.val().pubKey;
+                startChat(firebase.auth().currentUser(), userkey, userPubKey);
+            }
+        }).catch( (error) => {
+            console.log(error.message);
+            console.log(error.code);
+        });
     }
+
+
 }
-function splitter() {
-    parseSearchedEmails();
-    showPassPhraseInput();
-}
+//SPLITTER AND ENCRYPTION KEY REMOVED
 function sendMessage(user, userPubKey, userkey) { //TEMPORARY. NOT TO BE IMPLEMENTED WITHOUT TEJAS'S APPROVAL
     var date = new Date();
     var timestamp = date.getTime();
-    var localuuid = firebase.auth().currentUser().uid;
     var localEmail = firebase.auth().currentUser().email;
-    var position = database.ref("/users/emailConv/" + localEmail + "/chats").once(targetUID);
+    var position = database.ref("/users/emailConv/" + localEmail + "/chats/" + targetUID).once();
     if (position) {
         database.ref("/chats/" + localuuid + " " + targetUID + "/" + localuuid + "/messages/" + timestamp).set({
             "date": date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate(),
